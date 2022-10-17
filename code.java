2ad343c96bcfd29c31596b83d3efbffbddd22062
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-17 23:55:18 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int addNumbers(int n);
+
+int main() {
+
+  int num;
+  printf("Enter a positive integer: ");
+  scanf("%d", &num);
+  printf("Sum = %d", addNumbers(num));
+  return 0;
+}
+
+//help in logic building
+}
